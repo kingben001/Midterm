4d38b2339b0fba53b21700b2d4fefe7aae57fbcc
kingben001 <kingben001@yahoo.com>
2022-09-05 14:23:14 -0400

    Initial commit

---
+++ src/com/company/Quiz.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Quiz {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+
+        public void add(){
+            //does stuff
+        }
+
+        public void giveQuiz(){
+            //likely prints the questions
+            //then takes user input to check if it's right
+        }
+
+    }
